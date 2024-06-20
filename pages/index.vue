f757dde133d3351f5970c33d87c6f8764e3ff9d2
alexx <als698@users.noreply.github.com>
2024-06-20 17:24:11 +0200

    Dashboard

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import DashboardComponent from "../components/dashboard/DashboardComponent.vue";
+
+useHead({
+  title: "ATC Dashboard",
+});
+</script>
+
+<template>
+  <DashboardComponent />
+</template>
+
+<style lang="scss" scoped></style>
