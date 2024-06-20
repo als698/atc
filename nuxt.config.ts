6985cdba3b30a509d6503262b32aa821b2b2f6c7
alexx <als698@users.noreply.github.com>
2024-06-20 17:27:06 +0200

    nuxt.config.ts: ssr, css, modules, googleFonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: false,
+  css: ["~/assets/scss/main.scss"],
+  modules: ["nuxt-icon", "@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
+  googleFonts: {
+    families: {
+      Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+  },
+  devtools: { enabled: true },
+});
